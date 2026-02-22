09f6dbeb889f56f518e83e93e49046c738d63d61
Iryna Dmytrenko <10i07r19a94@ukr.net>
2026-02-22 18:00:19 +0200

    Fix imports after moving App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -36,13 +36,13 @@
 
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import type { Movie } from "./types/movie";
-import { fetchMovies } from "./services/movieService";
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import type { Movie } from "../../types/movie";
+import { fetchMovies } from "../../services/movieService";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
   const [movies, setMovies] = useState<Movie[]>([]);
