17f8684e53832da745ff6b508766c15648704191
Iryna Dmytrenko <10i07r19a94@ukr.net>
2026-02-22 17:38:38 +0200

    Fix type import for Movie

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@
 
 import { useState } from "react";
 import { Toaster } from "react-hot-toast";
-import { Movie } from "./types/movie";
+import type { Movie } from "./types/movie";
 import { fetchMovies } from "./services/movieService";
 import SearchBar from "./components/SearchBar/SearchBar";
 import MovieGrid from "./components/MovieGrid/MovieGrid";
